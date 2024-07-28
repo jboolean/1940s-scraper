27d095049484cd1aca00862475db73e2d51707fa
Julian Boilen <julianboilen@gmail.com>
2024-07-28 14:15:33 -0400

    Implement data fetching with pptr instead of axios
    Includes captcha solving, and generally looks more realistic, harder to block

---
+++ browser/normalize-ua.js
@@ -0,0 +1,24 @@
+const puppeteer = require("puppeteer");
+
+const normalizeUserAgent = () => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let browser = await puppeteer.launch({
+        headless: true,
+        args: ["--no-sandbox"],
+      });
+      let userAgent = await browser.userAgent();
+      let normalized = userAgent.replace("Headless", "");
+      normalized = normalized.replace("Chromium", "Chrome");
+      await browser.close();
+      resolve(normalized);
+    } catch (e) {
+      console.error("Could not normalize ua", e);
+      resolve(
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
+      );
+    }
+  });
+};
+
+module.exports = { normalizeUserAgent };
